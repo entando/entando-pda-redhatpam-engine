6e511193247c20b6d1d362da64fd1931c52045cd
Werbth Rocha <werbth@gmail.com>
2020-02-20 23:59:00 -0300

    PDA-118: Improve date deserializer

--- src/main/java/org/entando/plugins/pda/pam/service/task/KieDateDeserializer.java
+++ src/main/java/org/entando/plugins/pda/pam/service/task/KieDateDeserializer.java
@@ -5,17 +5,37 @@ import com.fasterxml.jackson.core.TreeNode;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.node.LongNode;
+import com.fasterxml.jackson.databind.node.TextNode;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class KieDateDeserializer extends JsonDeserializer<Date> {
 
     private static final String DATE_KEY = "java.util.Date";
 
+    private final String dateFormatStr = System
+            .getProperty("org.kie.server.json.date_format", "yyyy-MM-dd'T'hh:mm:ss.SSSZ");
+    private final DateFormat dateFormat = new SimpleDateFormat(dateFormatStr, Locale.US);
+
     @Override
     public Date deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
-        TreeNode jsonObject = jsonParser.getCodec().readTree(jsonParser);
-        LongNode treeNode = (LongNode) jsonObject.get(DATE_KEY);
-        return new Date(treeNode.longValue());
+        TreeNode node = jsonParser.getCodec().readTree(jsonParser);
+        try {
+            if (node.isObject()) {
+                LongNode treeNode = (LongNode) node.get(DATE_KEY);
+                return new Date(treeNode.longValue());
+            } else {
+                return dateFormat.parse(((TextNode) node).asText());
+            }
+        } catch (ParseException e) {
+            log.error("Error deserializing date attribute.", e);
+        }
+        return null;
     }
 }
