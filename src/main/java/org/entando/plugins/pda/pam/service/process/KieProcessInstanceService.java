8e9575da2e1811293fcb49d14e83067bf90a8fd2
Werbth Rocha <werbth@gmail.com>
2020-05-13 03:53:13 -0300

    PDA-234: Improve tests

--- src/main/java/org/entando/plugins/pda/pam/service/process/KieProcessInstanceService.java
+++ src/main/java/org/entando/plugins/pda/pam/service/process/KieProcessInstanceService.java
@@ -1,7 +1,13 @@
 package org.entando.plugins.pda.pam.service.process;
 
+import static org.entando.plugins.pda.pam.service.task.model.KieTask.KIE_STATUS_CREATED;
+import static org.entando.plugins.pda.pam.service.task.model.KieTask.KIE_STATUS_IN_PROGRESS;
+import static org.entando.plugins.pda.pam.service.task.model.KieTask.KIE_STATUS_READY;
+import static org.entando.plugins.pda.pam.service.task.model.KieTask.KIE_STATUS_RESERVED;
+
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -20,6 +26,10 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class KieProcessInstanceService implements ProcessInstanceService {
 
+    public static final int ALL_ITEMS = -1;
+    public static final List<String> ACTIVE_STATUSES = Collections.unmodifiableList(
+            Arrays.asList(KIE_STATUS_CREATED, KIE_STATUS_READY, KIE_STATUS_RESERVED, KIE_STATUS_IN_PROGRESS));
+
     private final KieApiService kieApiService;
 
     @Override
@@ -30,7 +40,7 @@ public class KieProcessInstanceService implements ProcessInstanceService {
         QueryServicesClient queryServicesClient = kieApiService.getQueryServicesClient(connection);
         UserTaskServicesClient userTaskServicesClient = kieApiService.getUserTaskServicesClient(connection);
         return queryServicesClient
-                .findProcessInstancesByProcessIdAndInitiator(processDefinitionId, username, null, 0, -1)
+                .findProcessInstancesByProcessIdAndInitiator(processDefinitionId, username, null, 0, ALL_ITEMS)
                 .stream()
                 .map((org.kie.server.api.model.instance.ProcessInstance pi) -> toProcessInstance(userTaskServicesClient,
                         pi))
@@ -40,7 +50,7 @@ public class KieProcessInstanceService implements ProcessInstanceService {
     private ProcessInstance toProcessInstance(UserTaskServicesClient userTaskServicesClient,
             org.kie.server.api.model.instance.ProcessInstance processInstance) {
         List<TaskSummary> taskSummaries = userTaskServicesClient
-                .findTasksByStatusByProcessInstanceId(processInstance.getId(), null, 0, -1);
+                .findTasksByStatusByProcessInstanceId(processInstance.getId(), ACTIVE_STATUSES, 0, ALL_ITEMS);
         LocalDateTime date = processInstance.getDate() == null ? null
                 : processInstance.getDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
         return ProcessInstance.builder()
--- src/test/java/org/entando/plugins/pda/pam/service/process/KieProcessInstanceServiceTest.java
+++ src/test/java/org/entando/plugins/pda/pam/service/process/KieProcessInstanceServiceTest.java
@@ -1,26 +1,30 @@
 package org.entando.plugins.pda.pam.service.process;
 
+import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
+import static org.apache.commons.lang3.RandomStringUtils.randomNumeric;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
+import static org.entando.plugins.pda.pam.service.process.KieProcessInstanceService.ACTIVE_STATUSES;
+import static org.entando.plugins.pda.pam.service.process.KieProcessInstanceService.ALL_ITEMS;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
-import org.apache.commons.lang3.RandomStringUtils;
+import java.util.stream.Collectors;
 import org.entando.plugins.pda.core.engine.Connection;
 import org.entando.plugins.pda.core.model.ProcessInstance;
 import org.entando.plugins.pda.core.utils.TestUtils;
 import org.entando.plugins.pda.pam.service.api.KieApiService;
-import org.entando.plugins.pda.pam.service.task.model.KieTask;
 import org.junit.Before;
 import org.junit.Test;
 import org.kie.server.api.model.instance.TaskSummary;
-import org.kie.server.api.model.instance.TaskSummaryList;
 import org.kie.server.client.QueryServicesClient;
 import org.kie.server.client.UserTaskServicesClient;
 
@@ -43,26 +47,18 @@ public class KieProcessInstanceServiceTest {
     }
 
     @Test
-    public void shouldListProcessInstancesByUser() {
+    public void shouldListProcessInstances() {
         // Given
-        org.kie.server.api.model.instance.ProcessInstance kieProcessInstance =
-                new org.kie.server.api.model.instance.ProcessInstance();
-        kieProcessInstance.setContainerId(RandomStringUtils.randomAlphabetic(10));
-        kieProcessInstance.setId(Long.valueOf(RandomStringUtils.randomNumeric(5)));
-        kieProcessInstance.setProcessId(RandomStringUtils.randomAlphabetic(10));
-        TaskSummary task = new TaskSummary();
-        task.setId(Long.valueOf(RandomStringUtils.randomNumeric(5)));
-        task.setName(RandomStringUtils.randomAlphabetic(10));
-        task.setStatus(KieTask.KIE_STATUS_CREATED);
-        task.setContainerId(RandomStringUtils.randomAlphabetic(10));
-        List<TaskSummary> tasks = Collections.singletonList(task);
-        TaskSummaryList taskSummaryList = new TaskSummaryList(tasks);
-        kieProcessInstance.setActiveUserTasks(taskSummaryList);
+        org.kie.server.api.model.instance.ProcessInstance kieProcessInstance1 = getKieProcessInstance();
+        org.kie.server.api.model.instance.ProcessInstance kieProcessInstance2 = getKieProcessInstance();
         String processDefinitionId = "myProcess";
         when(queryServicesClient
                 .findProcessInstancesByProcessIdAndInitiator(eq(processDefinitionId), eq(TEST_USER), eq(null), anyInt(),
-                        anyInt())).thenReturn(Collections.singletonList(kieProcessInstance));
-        when(userTaskServicesClient.findTasksByStatusByProcessInstanceId(anyLong(), eq(null), anyInt(), anyInt()))
+                        anyInt())).thenReturn(Arrays.asList(kieProcessInstance1, kieProcessInstance2));
+        List<TaskSummary> tasks = Arrays.asList(
+                TaskSummary.builder().name(randomAlphabetic(10)).build(),
+                TaskSummary.builder().name(randomAlphabetic(10)).build());
+        when(userTaskServicesClient.findTasksByStatusByProcessInstanceId(anyLong(), any(), anyInt(), anyInt()))
                 .thenReturn(tasks);
 
         // When
@@ -71,9 +67,25 @@ public class KieProcessInstanceServiceTest {
 
         // Then
         verify(queryServicesClient)
-                .findProcessInstancesByProcessIdAndInitiator(processDefinitionId, TEST_USER, null, 0, -1);
-        verify(userTaskServicesClient).findTasksByStatusByProcessInstanceId(anyLong(), eq(null), eq(0), eq(-1));
-        assertThat(kieProcessInstance.getActiveUserTasks().getItems().get(0).getName())
-                .isEqualTo(processInstances.get(0).getActiveUserTasks().get(0));
+                .findProcessInstancesByProcessIdAndInitiator(processDefinitionId, TEST_USER, null, 0, ALL_ITEMS);
+        verify(userTaskServicesClient, times(2)).findTasksByStatusByProcessInstanceId(anyLong(),
+                eq(ACTIVE_STATUSES), eq(0), eq(ALL_ITEMS));
+        assertThat(processInstances).extracting(ProcessInstance::getId, ProcessInstance::getProcessName)
+                .containsExactlyInAnyOrder(
+                        tuple(String.valueOf(kieProcessInstance1.getId()), kieProcessInstance1.getProcessName()),
+                        tuple(String.valueOf(kieProcessInstance2.getId()), kieProcessInstance2.getProcessName()));
+        List<String> taskNames = tasks.stream().map(TaskSummary::getName).collect(Collectors.toList());
+        assertThat(processInstances.get(0).getActiveUserTasks()).containsExactlyElementsOf(taskNames);
+        assertThat(processInstances.get(1).getActiveUserTasks()).containsExactlyElementsOf(taskNames);
+    }
+
+    private org.kie.server.api.model.instance.ProcessInstance getKieProcessInstance() {
+        org.kie.server.api.model.instance.ProcessInstance kieProcessInstance =
+                new org.kie.server.api.model.instance.ProcessInstance();
+        kieProcessInstance.setContainerId(randomAlphabetic(10));
+        kieProcessInstance.setId(Long.valueOf(randomNumeric(5)));
+        kieProcessInstance.setProcessId(randomAlphabetic(10));
+        kieProcessInstance.setProcessName(randomAlphabetic(10));
+        return kieProcessInstance;
     }
 }
